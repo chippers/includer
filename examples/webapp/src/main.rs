ae926b26b87cb1101b86f2d377c16c9cd35913f1
chip reed <chip@chip.sh>
2018-09-14 09:12:48 -0700

    update example webapp tests

--- examples/webapp/src/main.rs
+++ examples/webapp/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
 ///
 /// friendly reminder: usually you wouldn't be searching for your manifest
 /// (thinking of index.html as a manifest of your webapp assets) in your code,
-/// ideally you should know the manifest beforehand and possibly keep it
+/// ideally you should know the manifest beforehand and even possibly keep it
 /// separate from your other included assets.
 pub fn index() -> &'static [u8] {
     let idx = find_index_index(&ASSETS); // see function comment
@@ -35,7 +35,6 @@ fn find_index_index(assets: &[Asset]) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use super::{index, ASSETS};
     use select::document::Document;
     use select::node::Node;
     use select::predicate::Name;
@@ -43,24 +42,24 @@ mod tests {
 
     #[test]
     fn index_exists() {
-        let index_bytes = index();
-        assert!(index_bytes.len() > 0)
+        let index = super::index();
+        assert!(index.len() > 0)
     }
 
-    // friendly reminder: this isn't a good test for an actual production
-    // environment, its a very limited scoped test that assumes that there
-    // is exactly 1 <script> html tag in `/index.html`.  Testing for amount of
-    // external scripts/styles can have its place in a real codebase, but this
-    // is a very narrow test of exactly what the example webapp is supposed to
-    // show. if you know exactly what this entails and you understand why this
-    // might be an applicable test for your codebase, ignore this but it should
-    // be a well understood problem.  please open an issue if you may have a
-    // better way of showing this.
+    /// friendly reminder: this isn't a good test for an actual production
+    /// environment, its a very limited scoped test that assumes that there
+    /// is exactly 1 <script> html tag in `/index.html`.  Testing for amount of
+    /// external scripts/styles can have its place in a real codebase, but this
+    /// is a very narrow test of exactly what the example webapp is supposed to
+    /// show. if you know exactly what this entails and you understand why this
+    /// might be an applicable test for your codebase, ignore this but it
+    /// should be a well understood problem.  please open an issue if you may
+    /// have a better way of showing this.
     #[test]
     fn index_requires_included_assets() {
-        let actual_index = index();
-        let index_str = str::from_utf8(&actual_index).unwrap();
-        let document = Document::from(index_str);
+        let index = super::index();
+        let index_s = str::from_utf8(&index).unwrap();
+        let document = Document::from(index_s);
 
         // note that this isn't necessarily a good test for a production
         // codebase, this is just for this simple example test.
@@ -70,8 +69,8 @@ mod tests {
         // testing that we have the specific js asset that is required by
         // index.html in our included assets.  reminder: yada yada yada not
         // for production unless you really know what you're doing.
-        let js_script = scripts[0];
-        let src_str = js_script.attr("src").unwrap();
-        assert!(ASSETS.iter().any(|asset| asset.uri() == src_str));
+        let js = scripts[0];
+        let src = js.attr("src").unwrap();
+        assert!(super::ASSETS.iter().any(|asset| asset.uri() == src));
     }
 }
